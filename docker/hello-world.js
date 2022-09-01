84b016da7474943621396c8bdcf7dfd696f5999b
unknown <{ID}+{username}@users.noreply.github.com>
2022-09-01 03:02:24 +0300

    update

--- docker/hello-world.js
+++ docker/hello-world.js
@@ -1,24 +1,37 @@
 const express = require("express");
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
-
+const fs = require("fs");
+const path = require("path");
 const app = express();
 const ENV = process.env.ENV || "deafult";
 const PORT = process.env.PORT || 3000;
 const KUBE_ENV_TEST = process.env.KUBE_ENV_TEST || "";
+const MOUNT_PATH = process.env.MOUNT_PATH || "";
+const fileLocation = path.join(MOUNT_PATH, "tmp.json");
 
 app.get("/health", (req, res) => {
   console.log(" health request");
   return res.status(200).json({ status: "UP", KUBE_ENV_TEST, ENV });
-
-  // process.exit(0);
 });
 
 app.get("/check", (req, res) => {
   console.log("got request");
-  res.status(400).json(`hello-world-kubernates2 in ${ENV}!!`);
+  const file = JSON.parse(fs.readFileSync(fileLocation, "utf8"));
+  return res.status(200).json(file);
+});
+app.get("/add", (req, res) => {
+  console.log("got request");
+  const file = JSON.parse(fs.readFileSync(fileLocation, "utf8"));
+  file.counter = file.counter + 1;
+  fs.writeFileSync(fileLocation, JSON.stringify(file), "utf8");
 
-  // process.exit(0);
-  return;
+  return res.status(200).json(file);
+});
+app.post("/write", (req, res) => {
+  console.log("got request");
+  fs.writeFileSync(fileLocation, JSON.stringify({ counter: 0 }), "utf8");
+
+  return res.status(200).json();
 });
 
 app.listen(PORT, () => {
