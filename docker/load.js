c57714aa387c222571d63969c1918916103d7bbc
unknown <{ID}+{username}@users.noreply.github.com>
2022-09-01 23:59:29 +0300

    update

---
+++ docker/load.js
@@ -0,0 +1,9 @@
+const index = parseInt(process.argv[2]);
+const arr = [];
+
+for (let i = 0; i < index; i++) {
+  arr.push(i);
+  arr.includes("i");
+  //   console.log(i + 500 * 1);
+}
+arr.concat(arr).reverse().reverse().reverse().reverse().reverse().reverse();
--- docker/server.js
+++ docker/server.js
@@ -2,6 +2,11 @@ const express = require("express");
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 const fs = require("fs");
 const path = require("path");
+// const util = require('util');
+const { exec } = require("child_process");
+
+// const exec = util.promisify(child.exec);
+// const result = await exec(`my command`);
 const app = express();
 const ENV = process.env.ENV || "deafult";
 const PORT = process.env.PORT || 3000;
@@ -19,12 +24,12 @@ app.get("/health", (req, res) => {
 });
 
 app.get("/getCounter", (req, res) => {
-  console.log("got request");
+  console.log("getCounter request");
   const file = JSON.parse(fs.readFileSync(fileLocation, "utf8"));
   return res.status(200).json(file);
 });
 app.put("/addCounter", (req, res) => {
-  console.log("got request");
+  console.log("addCounter request");
   const file = JSON.parse(fs.readFileSync(fileLocation, "utf8"));
   file.counter = file.counter + 1;
   fs.writeFileSync(fileLocation, JSON.stringify(file), "utf8");
@@ -32,12 +37,29 @@ app.put("/addCounter", (req, res) => {
   return res.status(200).json(file);
 });
 app.post("/writeFile", (req, res) => {
-  console.log("got request");
+  console.log("writeFile request");
   fs.writeFileSync(fileLocation, JSON.stringify({ counter: 0 }), "utf8");
 
   return res.status(200).json();
 });
 
+app.post("/makeLoad/:load", (req, res) => {
+  const laod = req.params.load;
+  console.log("makeLoad request" + laod);
+
+  // return res.status(200).json({ user: usage.user, system: usage.system });
+  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].forEach(() => {
+    exec("node load.js " + laod, (err, stdout, stderr) => {
+      if (err) {
+        // node couldn't execute the command
+        return;
+      } else {
+      }
+    });
+  });
+  var usage = process.cpuUsage();
+  res.status(200).json(usage);
+});
 app.listen(PORT, () => {
   console.log("listen on port 3000!");
 });
