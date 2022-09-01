8a7eafc13c80c6d9de16a3e971a3327adbac291c
unknown <{ID}+{username}@users.noreply.github.com>
2022-09-01 10:19:37 +0300

    add secret

--- docker/server.js
+++ docker/server.js
@@ -7,19 +7,23 @@ const ENV = process.env.ENV || "deafult";
 const PORT = process.env.PORT || 3000;
 const KUBE_ENV_TEST = process.env.KUBE_ENV_TEST || "";
 const MOUNT_PATH = process.env.MOUNT_PATH || "";
+const USERNAME = process.env.USERNAME || "";
+const PASSWORD = process.env.PASSWORD || "";
+const SERVICE_NAME = process.env.SERVICE_NAME || "";
+
 const fileLocation = path.join(MOUNT_PATH, "tmp.json");
 
 app.get("/health", (req, res) => {
   console.log(" health request");
-  return res.status(200).json({ status: "UP", KUBE_ENV_TEST, ENV });
+  return res.status(200).json({ status: "UP", SERVICE_NAME, ENV, KUBE_ENV_TEST, USERNAME, PASSWORD });
 });
 
-app.get("/check", (req, res) => {
+app.get("/getCounter", (req, res) => {
   console.log("got request");
   const file = JSON.parse(fs.readFileSync(fileLocation, "utf8"));
   return res.status(200).json(file);
 });
-app.get("/add", (req, res) => {
+app.put("/addCounter", (req, res) => {
   console.log("got request");
   const file = JSON.parse(fs.readFileSync(fileLocation, "utf8"));
   file.counter = file.counter + 1;
@@ -27,7 +31,7 @@ app.get("/add", (req, res) => {
 
   return res.status(200).json(file);
 });
-app.post("/write", (req, res) => {
+app.post("/writeFile", (req, res) => {
   console.log("got request");
   fs.writeFileSync(fileLocation, JSON.stringify({ counter: 0 }), "utf8");
 
