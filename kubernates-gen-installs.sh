a595ece06f071ef739fc4456c495a7cfd0f762f6
unknown <{ID}+{username}@users.noreply.github.com>
2022-10-01 21:39:56 +0300

    add ingress

---
+++ kubernates-gen-installs.sh
@@ -0,0 +1,2 @@
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.3.1/deploy/static/provider/cloud/deploy.yaml
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
