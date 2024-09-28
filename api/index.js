dc9f63802e1585aa9f84ecc08c68757fd8141609
unknown <{ID}+{username}@users.noreply.github.com>
2024-09-28 22:08:35 +0300

    update

--- api/index.js
+++ api/index.js
@@ -1,14 +1,14 @@
 const fs = require("fs");
 const axios = require("axios");
 
-const list = fs.readdirSync(".").filter((name) => name.endsWith(".yaml"));
+const list = fs.readdirSync("../").filter((name) => name.endsWith(".yaml"));
 const files = [];
 
 for (let index = 0; index < list.length; index++) {
   const fileName = list[index];
   const obj = {
     fileName,
-    fileContent: fs.readFileSync(`./${fileName}`, "base64"),
+    fileContent: fs.readFileSync(`../${fileName}`, "base64"),
   };
   files.push(obj);
 }
